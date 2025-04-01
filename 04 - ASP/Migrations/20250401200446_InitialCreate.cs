c34b882e84348eb8810ea529d2ec3c904bfd8e30
Célia JOSSO <70684974+celiajosso@users.noreply.github.com>
2025-04-01 22:24:11 +0200

    ASP suffcient task finished

---
+++ 04 - ASP/Migrations/20250401200446_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Recipe",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Time = table.Column<int>(type: "INTEGER", nullable: true),
+                    Difficulty = table.Column<string>(type: "TEXT", nullable: true),
+                    Likes = table.Column<int>(type: "INTEGER", nullable: true),
+                    Ingredients = table.Column<string>(type: "TEXT", nullable: true),
+                    Process = table.Column<string>(type: "TEXT", nullable: true),
+                    Tips = table.Column<string>(type: "TEXT", nullable: true),
+                    Tricks = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Recipe", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Recipe");
+        }
+    }
+}
